920d2147ab7cb7f6437f282eb309633bdf2f1ba8
Ron Merom <ron@wetrust.io>
2017-02-17 17:48:22 +0000

    remove MINIMUM_TIME_BEFORE_ROSCA_STARTS

--- contracts/ROSCA.sol
+++ contracts/ROSCA.sol
@@ -22,9 +22,6 @@ contract ROSCA {
   // Maximum fee (in 1/1000s) from dispersements that is shared between foreperson and other stakeholders..
   uint16 constant internal MAX_FEE_IN_THOUSANDTHS = 20;
 
-  // Start time of the ROSCA must be at least this much time ahead of when the ROSCA is created
-  uint32 constant internal MINIMUM_TIME_BEFORE_ROSCA_START = 1 days;
-
   uint8 constant internal MIN_ROUND_PERIOD_IN_DAYS = 1;
   uint8 constant internal MAX_ROUND_PERIOD_IN_DAYS = 30;
   // the winning bid must be at least this much of the maximum (aka default) pot value
@@ -184,9 +181,6 @@ contract ROSCA {
     }
     contributionSize = contributionSize_;
 
-    if (startTime_ < (now + MINIMUM_TIME_BEFORE_ROSCA_START)) {
-      throw;
-    }
     startTime = startTime_;
     if (serviceFeeInThousandths_ > MAX_FEE_IN_THOUSANDTHS) {
       throw;
